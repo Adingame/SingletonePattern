a270462b1ee63e7d32fa727f1cd82912a1633cf6
Adingame <45685592+Adingame@users.noreply.github.com>
2020-12-28 18:36:37 +0600

    Singletoe realization

---
+++ HomeWork/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singletone MyData = Singletone.CreateOrGetInstance();
+
+            MyData.FillLstData();
+
+            int sumFirstObject = MyData.LstDataSum();
+
+            Singletone MySecondData = Singletone.CreateOrGetInstance();
+
+            int sumSecondObject = MySecondData.LstDataSum();
+
+            if(sumFirstObject == sumSecondObject)
+            {
+                Console.WriteLine("Its 1 object");
+            }else
+            {
+                Console.WriteLine("Its not 1 object");
+            }
+        }
+    }
+}
---
+++ HomeWork/Singletone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork
+{
+    public class Singletone
+    {
+        public static Singletone Instance;
+        public static List<int> Lst = new List<int>();
+        public static Singletone CreateOrGetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new Singletone();
+
+                return Instance;
+            }
+            else
+            {
+                return Instance;
+            }
+        }
+        private Singletone() { }
+        public void FillLstData()
+        {
+            Random random = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                Lst.Add(random.Next(-1000, 1000));
+            }
+        }
+        public int LstDataSum()
+        {
+            int sum = 0;
+            foreach(var i in Lst)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
